49cbbb7a7e101b93aee90672ae44941f7c20e804
kavyavenkatesh-10027 <kavyavenkatesh2007@gmail.com>
2026-07-09 11:01:27 +0530

    Commit

--- src/Main.kt
+++ src/Main.kt
@@ -4,8 +4,8 @@ import java.util.Scanner
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val adminCaller = AdminUI(Scanner(System.`in`))
+    val adminCaller = AdminUI()
     adminCaller.show()
-    val consumerCaller = ConsumerUI(Scanner(System.`in`))
+    val consumerCaller = ConsumerUI()
     consumerCaller.show()
 }
