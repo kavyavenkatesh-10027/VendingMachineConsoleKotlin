8653c50217ec9245f9235a2a3ab589910463e167
kavyavenkatesh-10027 <kavyavenkatesh2007@gmail.com>
2026-07-15 12:59:51 +0530

    Commit

--- src/util/VendingMachineException.kt
+++ src/util/VendingMachineException.kt
@@ -1,3 +1,25 @@
 package util
 
-class VendingMachineException(message: String) : RuntimeException(message)
+open class VendingMachineException(message: String) : RuntimeException(message)
+
+class CurrencyHandlingException(message: String)  : VendingMachineException(message)
+
+class PurchaseHandlingException(message: String)  : VendingMachineException(message)
+
+class FoodHandlingException(message: String)  : VendingMachineException(message)
+
+class SlotHandlingException(message: String)  : VendingMachineException(message)
+
+class VMHandlingException(message: String)  : VendingMachineException(message)
+//Service wise
+
+class EntityDoesNotExistException(message: String) : VendingMachineException(message)
+
+class InsufficientStockException(message: String) : VendingMachineException(message)
+
+class InsufficientCashInputException(message: String) : VendingMachineException(message)
+
+class InsufficientDenominationTypesException(message: String) : VendingMachineException(message)
+
+//Utility wise
+//Exceptions
